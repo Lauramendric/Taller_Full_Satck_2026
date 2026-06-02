0fa173873ff80e820881d3c9aa55b985e9c90527
Lauramendric <arual.mendoza@gmail.com>
2026-06-02 17:35:13 -0600

    Mi avance del proyecto

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
+    password: "Trabajo.26",
     database: "contactos_db"
 });
 
